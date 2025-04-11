46f2c2d251d7dd318374efdc019e9e7f96a147d3
Ange-09 <santiago.ry.angelo@gmail.com>
2025-04-11 15:58:31 +0800

    with variables

---
+++ calculatorscript.js
@@ -0,0 +1,37 @@
+// Global Variables
+let rf, temp, whac;
+let bod, disox, fc, ph, ammonia, nitrate, phosphate;
+let bod_transformed, do_transformed, fc_transformed, ph_transformed, ammonia_transformed, nitrate_transformed, phosphate_transformed;
+let FClaguna, FCrizal, FCtaguig;
+
+function calculatewaterquality ()
+{
+    // Get the values from the input fields
+    rf = parseFloat(document.getElementById("rf").value);
+    temp = parseFloat(document.getElementById("temp").value);
+    whac = parseFloat(document.getElementById("whac").value);
+
+    // First Phase: Calculate Water Quality
+    // Untransformed
+    bod = 
+    disox = 
+    fc = 
+    ph = 
+    ammonia = 
+    nitrate = 
+    phosphate = 
+
+    // Transformed
+    bod_transformed =
+    do_transformed =
+    fc_transformed =
+    ph_transformed =
+    ammonia_transformed =
+    nitrate_transformed =
+    phosphate_transformed =
+
+    // Second Phase: Calculate Fish Count
+    FClaguna = 
+    FCrizal = 
+    FCtaguig = 
+}
