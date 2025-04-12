74f13b2756a7866df98659f4390061a45543567a
Ange-09 <santiago.ry.angelo@gmail.com>
2025-04-12 19:27:47 +0800

    working average

--- calculatorscript.js
+++ calculatorscript.js
@@ -18,3 +18,44 @@ function updateInputFields() {
 window.onload = function() {
     updateInputFields();
 };
+
+function calculate(){
+    let rfAvg, tempAvg, whacAvg;
+    let rfm1,tempm1, whacm1;
+    let rfm2,tempm2, whacm2;
+    let rfm3,tempm3, whacm3;
+    let selection = document.getElementById("dataRange").value;
+
+
+    //Input Variables
+    rfAvg = parseFloat(document.getElementById("rfAvg").value);
+    tempAvg = parseFloat(document.getElementById("tempAvg").value);
+    whacAvg = parseFloat(document.getElementById("whacAvg").value);
+
+    rfm1 = parseFloat(document.getElementById("rfm1").value);
+    tempm1 = parseFloat(document.getElementById("tempm1").value);
+    whacm1 = parseFloat(document.getElementById("whacm1").value);
+
+    rfm2 = parseFloat(document.getElementById("rfm2").value);
+    tempm2 = parseFloat(document.getElementById("tempm2").value);
+    whacm2 = parseFloat(document.getElementById("whacm2").value);
+
+    rfm3 = parseFloat(document.getElementById("rfm3").value);
+    tempm3 = parseFloat(document.getElementById("tempm3").value);
+    whacm3 = parseFloat(document.getElementById("whacm3").value);
+
+    if (selection === "average") {
+        computedrf = rfAvg
+        computedtemp = tempAvg
+        computedwhac = whacAvg
+    }
+    else if (selection === "period") {
+        computedrf = (rfm1 + rfm2 + rfm3) / 3
+        computedtemp = (tempm1 + tempm2 + tempm3) / 3
+        computedwhac = (whacm1 + whacm2 + whacm3) / 3
+    }
+
+    document.getElementById("computedrf").textContent = computedrf.toFixed(2);
+    document.getElementById("computedtemp").textContent = computedtemp.toFixed(2);
+    document.getElementById("computedwhac").textContent = computedwhac.toFixed(2);
+}
